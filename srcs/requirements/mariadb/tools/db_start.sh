92445ffa5410435879250ea7ad2690d092af690a
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-06-03 03:43:58 +0900

    fix: indent

--- srcs/requirements/mariadb/tools/db_start.sh
+++ srcs/requirements/mariadb/tools/db_start.sh
@@ -3,12 +3,12 @@
 # set -x
 
 if [ -d "/run/mysqld" ]; then
-	echo "[log info] : mysqld exists"
-	chown -R mysql:mysql /run/mysqld
+    echo "[log info] : mysqld exists"
+    chown -R mysql:mysql /run/mysqld
 else
-	echo "[log info] : mysqld not found ..."
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
+    echo "[log info] : mysqld not found ..."
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
 fi
 
 WORDPRESS_DB_NAME=${MYSQL_DATABASE}
@@ -18,27 +18,26 @@ MARIADB_USER=${MYSQL_USER}
 MARIADB_USER_PASS=${MYSQL_PASSWORD}
 
 if [ -d "/var/lib/mysql/mysql" ]; then
-	echo "[log info] : mysql directory exists"
-	chown -R mysql:mysql /var/lib/mysql/mysql
+    echo "[log info] : mysql directory exists"
+    chown -R mysql:mysql /var/lib/mysql/mysql
 else
-	echo "[log info] : mysql direcotry not found ..."
-	echo "[log info] : mysql installing ..."
-	echo "[log info] : \${MYSQL_ROOT_PASSWORD}   : ${MYSQL_ROOT_PASSWORD}"
-	echo "[log info] : \${WORDPRESS_DB_USER}     : ${WORDPRESS_DB_USER}"
-	echo "[log info] : \${WORDPRESS_DB_PASSWORD} : ${WORDPRESS_DB_PASSWORD}"
-	echo "[log info] : \${WORDPRESS_DB_NAME}     : ${WORDPRESS_DB_NAME}"
-	echo "[log info] : \${MARIADB_USER}          : ${MARIADB_USER}"
-	echo "[log info] : \${MARIADB_USER_PASS}     : ${MARIADB_USER_PASS}"
-	mkdir -p /var/lib/mysql/mysql
-	chown -R mysql:mysql /var/lib/mysql
-	# chown -R mysql:mysql /var/lib/mysql/mysql
-	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
+    echo "[log info] : mysql direcotry not found ..."
+    echo "[log info] : mysql installing ..."
+    echo "[log info] : \${MYSQL_ROOT_PASSWORD}   : ${MYSQL_ROOT_PASSWORD}"
+    echo "[log info] : \${WORDPRESS_DB_USER}     : ${WORDPRESS_DB_USER}"
+    echo "[log info] : \${WORDPRESS_DB_PASSWORD} : ${WORDPRESS_DB_PASSWORD}"
+    echo "[log info] : \${WORDPRESS_DB_NAME}     : ${WORDPRESS_DB_NAME}"
+    echo "[log info] : \${MARIADB_USER}          : ${MARIADB_USER}"
+    echo "[log info] : \${MARIADB_USER_PASS}     : ${MARIADB_USER_PASS}"
+    mkdir -p /var/lib/mysql/mysql
+    chown -R mysql:mysql /var/lib/mysql
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 
-	tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
-		return 1
-	fi
-	cat << EOF > $tfile
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
+    cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES ;
 GRANT ALL ON *.* TO 'root'@'%' identified by '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION ;
@@ -52,8 +51,8 @@ CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PASS}';
 GRANT ALL ON ${WORDPRESS_DB_NAME}.* to '${MARIADB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
-	rm -f $tfile
+    /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
+    rm -f $tfile
 fi
 
 exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
--- srcs/requirements/wordpress/tools/wp_start.sh
+++ srcs/requirements/wordpress/tools/wp_start.sh
@@ -9,47 +9,47 @@ WORDPRESS_ADMIN_USER_PASS=${WORDPRESS_ADMIN_USER_PASS}
 WORDPRESS_ADMIN_EMAIL=${WORDPRESS_ADMIN_EMAIL}
 
 if ! wp core is-installed --path=/var/www/html --allow-root &> /dev/null; then
-	echo "[log info] : \${WORDPRESS_DB_HOST}     : ${WORDPRESS_DB_HOST}"
-	echo "[log info] : \${WORDPRESS_DB_NAME}     : ${WORDPRESS_DB_NAME}"
-	echo "[log info] : \${WORDPRESS_DB_USER}     : ${WORDPRESS_DB_USER}"
-	echo "[log info] : \${WORDPRESS_DB_PASSWORD} : ${WORDPRESS_DB_PASSWORD}"
+    echo "[log info] : \${WORDPRESS_DB_HOST}     : ${WORDPRESS_DB_HOST}"
+    echo "[log info] : \${WORDPRESS_DB_NAME}     : ${WORDPRESS_DB_NAME}"
+    echo "[log info] : \${WORDPRESS_DB_USER}     : ${WORDPRESS_DB_USER}"
+    echo "[log info] : \${WORDPRESS_DB_PASSWORD} : ${WORDPRESS_DB_PASSWORD}"
 
-	echo "[log info] : downloading wordpress ..."
-	wp core download \
-		--locale=ja \
-		--path=/var/www/html \
-		--allow-root
+    echo "[log info] : downloading wordpress ..."
+    wp core download \
+        --locale=ja \
+        --path=/var/www/html \
+        --allow-root
 
-	echo "[log info] : creating wp-config.php ..."
-	wp config create \
-		--dbname=${WORDPRESS_DB_NAME} \
-		--dbuser=${WORDPRESS_DB_USER} \
-		--dbpass=${WORDPRESS_DB_PASSWORD} \
-		--dbhost=${WORDPRESS_DB_HOST} \
-		--allow-root \
-		--path=/var/www/html
+    echo "[log info] : creating wp-config.php ..."
+    wp config create \
+        --dbname=${WORDPRESS_DB_NAME} \
+        --dbuser=${WORDPRESS_DB_USER} \
+        --dbpass=${WORDPRESS_DB_PASSWORD} \
+        --dbhost=${WORDPRESS_DB_HOST} \
+        --allow-root \
+        --path=/var/www/html
 
-	echo "[log info] : installing wp-config.php ..."
-	wp core install \
-		--url=http://iyamada.42.fr \
-		--title=iyamada.42.fr \
-		--admin_user=${WORDPRESS_ADMIN_USER} \
-		--admin_password=${WORDPRESS_ADMIN_USER_PASS} \
-		--admin_email=${WORDPRESS_ADMIN_EMAIL} \
-		--allow-root \
-		--skip-email \
-		--path=/var/www/html
+    echo "[log info] : installing wp-config.php ..."
+    wp core install \
+        --url=http://iyamada.42.fr \
+        --title=iyamada.42.fr \
+        --admin_user=${WORDPRESS_ADMIN_USER} \
+        --admin_password=${WORDPRESS_ADMIN_USER_PASS} \
+        --admin_email=${WORDPRESS_ADMIN_EMAIL} \
+        --allow-root \
+        --skip-email \
+        --path=/var/www/html
 
-	echo "[log info] : creating user ..."
-	wp user create \
-		$WORDPRESS_AUTHOR_USER \
-		$WORDPRESS_AUTHOR_EMAIL \
-		--role=author \
-		--user_pass=$WORDPRESS_AUTHOR_USER_PASS \
-		--allow-root \
-		--path=/var/www/html
+    echo "[log info] : creating user ..."
+    wp user create \
+        $WORDPRESS_AUTHOR_USER \
+        $WORDPRESS_AUTHOR_EMAIL \
+        --role=author \
+        --user_pass=$WORDPRESS_AUTHOR_USER_PASS \
+        --allow-root \
+        --path=/var/www/html
 else
-	echo "[log info] : wordpress has already downloaded"
+    echo "[log info] : wordpress has already downloaded"
 fi
 
 echo "[log info] : now, wordpress container is running"
